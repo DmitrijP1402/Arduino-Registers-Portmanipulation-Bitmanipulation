d8a6afe255cf80a37f938dc15b8d36650852f2cc
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-02-06 19:00:18 +0100

    Update 3_Running_light_Uno.ino

--- 3_Running_light_Uno.ino
+++ 3_Running_light_Uno.ino
@@ -4,9 +4,9 @@ author:    "DmitrijP1402"
 filename:  "3_Running_light_Uno.ino"
 ********************************************/
 
-void setup () { 
-  DDRD = DDRD | B11111111; //Pin 7-0
-  DDRB = DDRB | B00111111; //Pin 13-8
+void setup () {
+  DDRD = DDRD | B11111111;  //Leds as OUTPUT at Pin 0-7
+  DDRB = DDRB | B00111111;  //Leds as OUTPUT at Pin 8-18
 }
 
 int time = 200;
@@ -20,102 +20,102 @@ void loop () {
   effectSix();
   effectSeven();
   effectEight();
-}  
+}
 
 void effectOne () {
   PORTB = B00111111;
   PORTD = B11111111;
-  delay(time); 
-  
+  delay(time);
+
   PORTB = B00000000;
   PORTD = B00000000;
-  delay(time); 
+  delay(time);
 }
 
-void effectTwo (){
+void effectTwo () {
   PORTD = B11111111;
   PORTB = B00111111;
-  
- for( int i= 0; i < 8; i++){
-  	PORTD = PORTD << 1;
-    delay(time);
- }
-  
- for( int i= 0; i < 6; i++){
-  	PORTB = PORTB << 1;
-    delay(time);
- }
-}
 
-void effectThree (){
-  PORTD = B11111111;
-  PORTB = B00111111;
-  
- for( int i= 0; i < 6; i++){
-  	PORTB = PORTB >> 1; 
+  for ( int i = 0; i < 8; i++) {
+    PORTD = PORTD << 1;
     delay(time);
- }
-  
- for( int i= 0; i < 8; i++){
-  	PORTD = PORTD >> 1; 
-    delay(time);
- }
-}
-
-void effectFour (){
- PORTD = B11111111;
- PORTB = B00111111;
-  
- for( int i= 0; i < 7; i++){
-  if (i ==0){
-   	PORTD = PORTD >> 1;
   }
-  	PORTD = PORTD >> 1; 
+
+  for ( int i = 0; i < 6; i++) {
+    PORTB = PORTB << 1;
+    delay(time);
+  }
+}
+
+void effectThree () {
+  PORTD = B11111111;
+  PORTB = B00111111;
+
+  for ( int i = 0; i < 6; i++) {
+    PORTB = PORTB >> 1;
+    delay(time);
+  }
+
+  for ( int i = 0; i < 8; i++) {
+    PORTD = PORTD >> 1;
+    delay(time);
+  }
+}
+
+void effectFour () {
+  PORTD = B11111111;
+  PORTB = B00111111;
+
+  for ( int i = 0; i < 7; i++) {
+    if (i == 0) {
+      PORTD = PORTD >> 1;
+    }
+    PORTD = PORTD >> 1;
     delay(time);
     PORTB = PORTB << 1;
- }
+  }
 }
 
-void effectFive (){
- PORTD = B11111111;
- PORTB = B00111111;
-   
- for( int i= 0; i < 7; i++){
-   if (i ==6){
-   	PORTD= PORTD << 1;
-   }
-   	PORTB = PORTB >> 1;
-  	PORTD = PORTD << 1; 
-    delay(time);
- }
-}
-
-void effectSix (){
+void effectFive () {
   PORTD = B11111111;
   PORTB = B00111111;
-  
- for( int i= 0; i < 4; i++){
-  	PORTD = PORTD << 2;
+
+  for ( int i = 0; i < 7; i++) {
+    if (i == 6) {
+      PORTD = PORTD << 1;
+    }
+    PORTB = PORTB >> 1;
+    PORTD = PORTD << 1;
     delay(time);
- }
-  
- for( int i= 0; i < 3; i++){
-  	PORTB = PORTB << 2;
-    delay(time);
- }
+  }
 }
 
-void effectSeven (){
+void effectSix () {
   PORTD = B11111111;
   PORTB = B00111111;
-  
- for( int i= 0; i < 3; i++){
-  	PORTB = PORTB >> 2; 
+
+  for ( int i = 0; i < 4; i++) {
+    PORTD = PORTD << 2;
     delay(time);
- }
-  
- for( int i= 0; i < 4; i++){
-  	PORTD = PORTD >> 2; 
+  }
+
+  for ( int i = 0; i < 3; i++) {
+    PORTB = PORTB << 2;
+    delay(time);
+  }
+}
+
+void effectSeven () {
+  PORTD = B11111111;
+  PORTB = B00111111;
+
+  for ( int i = 0; i < 3; i++) {
+    PORTB = PORTB >> 2;
+    delay(time);
+  }
+
+  for ( int i = 0; i < 4; i++) {
+    PORTD = PORTD >> 2;
     delay(time);
   }
 }
@@ -123,60 +123,60 @@ void effectSeven (){
 int b = 8;
 int a = 6;
 
-void effectEight(){
+void effectEight() {
 
-   if (b == 1){
-  	PORTD = B10000000;
-  	PORTB = B00000000;
+  if (b == 1) {
+    PORTD = B10000000;
+    PORTB = B00000000;
   }
-  
-  if (b == 2){
-  	PORTD = B01000000;
-  	PORTB = B00000000;
+
+  if (b == 2) {
+    PORTD = B01000000;
+    PORTB = B00000000;
   }
-  
-   if (b == 3){
-  	PORTD = B00100000;
-  	PORTB = B00000000;
+
+  if (b == 3) {
+    PORTD = B00100000;
+    PORTB = B00000000;
   }
-  
-  if (b == 4){
-  	PORTD = B00010000;
-  	PORTB = B00000000;
+
+  if (b == 4) {
+    PORTD = B00010000;
+    PORTB = B00000000;
   }
-  
-  if (b == 5){
-  	PORTD = B00001000;
-  	PORTB = B00000000;
+
+  if (b == 5) {
+    PORTD = B00001000;
+    PORTB = B00000000;
   }
-  
-  if (b == 6){
-  	PORTD = B00000100;
-  	PORTB = B00000000;
+
+  if (b == 6) {
+    PORTD = B00000100;
+    PORTB = B00000000;
   }
-  
-  if (b == 7){
-  	PORTD = B00000010;
-  	PORTB = B00000000;
+
+  if (b == 7) {
+    PORTD = B00000010;
+    PORTB = B00000000;
   }
-  
-  if (b == 8){
-  	PORTD = B00000001;
-  	PORTB = B00000000;
+
+  if (b == 8) {
+    PORTD = B00000001;
+    PORTB = B00000000;
   }
-  
-  for( int i= 0; i < b; i++){
+
+  for ( int i = 0; i < b; i++) {
     delay(time);
-  	PORTD = PORTD << 1; 
+    PORTD = PORTD << 1;
   }
-  
-  if (b == 8 || b == 7 || b == 6 || b ==5 || b == 4 || b == 3 || b ==2 || b == 1){
-  	PORTB = B00000001;
+
+  if (b == 8 || b == 7 || b == 6 || b == 5 || b == 4 || b == 3 || b == 2 || b == 1) {
+    PORTB = B00000001;
     b = b - 1;
   }
-  
-  for(int i =0; i < a; i++){
-  	delay(time);
-  	PORTB = PORTB << 1;
+
+  for (int i = 0; i < a; i++) {
+    delay(time);
+    PORTB = PORTB << 1;
   }
 }
