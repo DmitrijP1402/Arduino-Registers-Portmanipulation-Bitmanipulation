f18e64c786b8fa26f32fce7b22cbb98d5b15029f
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-02-06 18:56:55 +0100

    Update 1_blink_Uno.ino

--- 1_blink_Uno.ino
+++ 1_blink_Uno.ino
@@ -5,16 +5,16 @@ filename:  "1_blink_Uno.ino"
 ********************************************/
 
 void setup() {
-	DDRD = DDRD | B11000000; //Pin 7&6 D = 7-0
-	DDRB = DDRB |B00100000; //Pin 13 B = 8-13
+  DDRD = DDRD | B11000000; 	//Pin 7 & 6
+  DDRB = DDRB | B00100000; 	//Pin 13
 }
 
 void loop() {
-	  PORTD = B11000000;   // PIN 7 & 6 HIGH
-  	PORTB = B00100000;	// Pin 13 HIGH
-	  delay(1000);    
-	
-	  PORTD = B00000000; // Pin 7 & 6 LOW
-    PORTB = B00000000; // Pin 13 LOW
-	  delay(1000);// wait for a second
+  PORTD = B11000000;   	// PIN 7 & 6 HIGH
+  PORTB = B00100000;  	// Pin 13 HIGH
+  delay(1000);		// wait for a second
+
+  PORTD = B00000000; 	// Pin 7 & 6 LOW
+  PORTB = B00000000; 	// Pin 13 LOW
+  delay(1000);		// wait for a second
 }
