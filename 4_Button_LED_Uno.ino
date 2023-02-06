ac83154b2f61b5ae0e6e0bd997b4b28ac0f6f918
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-02-06 18:52:19 +0100

    Update 4_Button_LED_Uno.ino

--- 4_Button_LED_Uno.ino
+++ 4_Button_LED_Uno.ino
@@ -1,3 +1,9 @@
+/********************************************
+date:      "06.02.2023"
+authors:   "DmitrijP1402"
+filename:  "4_Button_LED_Uno.ino"
+********************************************/
+
 void setup() {
   DDRB = DDRB |B00100000; //LED as OUTPUT at Pin 13 LED
   DDRD = DDRD & B11111011; //Button as INPUT at pin 2
