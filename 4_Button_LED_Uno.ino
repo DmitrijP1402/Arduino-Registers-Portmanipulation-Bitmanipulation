6fec401a3b40821f4bcf8c0a0ac9ce5bc4f02e49
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-02-06 18:36:12 +0100

    Update and rename 4_Button_LED_Mega_2560.ino to 4_Button_LED_Uno.ino

---
+++ 4_Button_LED_Uno.ino
@@ -0,0 +1,15 @@
+void setup() {
+  DDRB = DDRB |B00100000; //LED as OUTPUT at Pin 13 LED
+  DDRD = DDRD & B11111011; //Button as INPUT at pin 2
+}
+
+void loop() {
+  bool buttonState = (PIND & (1 << PIND2)) >> PIND2; //Read the Button
+
+  if (buttonState == HIGH){ //if Button pressed
+    PORTB = B00100000; //LED HIGH
+  } 
+  else {
+    PORTB = B00000000; //LED LOW
+  }
+}
