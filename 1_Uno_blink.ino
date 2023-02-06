66426e17b76992c4b36915386cac764b495274fa
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-02-06 12:21:58 +0100

    Create 1_Uno_blink.ino

---
+++ 1_Uno_blink.ino
@@ -0,0 +1,14 @@
+void setup() {
+	DDRD = DDRD | B11000000; //Pin 7&6 D = 7-0
+	DDRB = DDRB |B00100000; //Pin 13 B = 8-13
+}
+
+void loop() {
+	  PORTD = B11000000;   // PIN 7 & 6 HIGH
+  	PORTB = B00100000;	// Pin 13 HIGH
+	  delay(1000);    
+	
+	  PORTD = B00000000; // Pin 7 & 6 LOW
+    PORTB = B00000000; // Pin 13 LOW
+	  delay(1000);// wait for a second
+}
