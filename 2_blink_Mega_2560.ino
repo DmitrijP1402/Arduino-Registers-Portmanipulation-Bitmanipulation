d13beb274f30a1aeae39b34a154a0b2a823cb9aa
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-02-06 18:58:10 +0100

    Update 2_blink_Mega_2560.ino

--- 2_blink_Mega_2560.ino
+++ 2_blink_Mega_2560.ino
@@ -4,14 +4,14 @@ author:    "DmitrijP1402"
 filename:  "2_blink_Mega_2560.ino"
 ********************************************/
 
-
 void setup() {
-	DDRD = DDRD | B10001100; //Leds as OUTPUT at Pin 38, 19 & 18 LED Mega 2560
+  DDRD = DDRD | B10001100;  //Leds as OUTPUT at Pin 38, 19 & 18
 }
 
 void loop() {
-	PORTD = B10001100;   //Led at Pin 38,19,18 HIGH
-	  delay(1000);    
-	PORTD = B00000000; //Leds at Pin 38,19,18 LOW
-    delay(1000);  
+  PORTD = B10001100;   //Led at Pin 38,19,18 HIGH
+  delay(1000);         // wait for a second
+  
+  PORTD = B00000000;  //Leds at Pin 38,19,18 LOW
+  delay(1000);        // wait for a second
 }
